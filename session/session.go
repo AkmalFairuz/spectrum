4e72eb04afaada2312fa32eba0242e58b7ada0e3
AkmalFairuz <akmalf475@gmail.com>
2025-06-21 23:47:51 +0700

    fix: simplify error logging in CloseWithError method

--- session/session.go
+++ session/session.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/google/uuid"
 	"github.com/sandertv/go-raknet"
 	"log/slog"
 	"reflect"
@@ -324,9 +323,7 @@ func (s *Session) Close() (err error) {
 
 func (s *Session) CloseWithError(err error) {
 	s.once.Do(func() {
-		errorID := uuid.New().String()[:8]
-		s.logger.Info("closed", "err_id", errorID, "err", err)
-		_ = s.client.WritePacket(&packet.Disconnect{Message: "error id: " + errorID})
+		_ = s.client.WritePacket(&packet.Disconnect{Message: err.Error()})
 		_ = s.client.Close()
 		s.processor.ProcessDisconnection(NewContext())
 		s.serverMu.RLock()
