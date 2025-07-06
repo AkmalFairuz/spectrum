f4c4e577691c88bdca8acb01d401b04edb0eb69a
AkmalFairuz <akmalf475@gmail.com>
2025-07-07 01:22:39 +0700

    fix: improve skin data validation logic for image dimensions and decoding

--- server/conn.go
+++ server/conn.go
@@ -417,7 +417,11 @@ func sanitizeClientData(cData login.ClientData) login.ClientData {
 	cData.SkinID = ""
 
 	parsedSkinData, parsedSkinDataErr := base64.StdEncoding.DecodeString(cData.SkinData)
-	if !((cData.SkinImageHeight == 128 && cData.SkinImageWidth == 128) || (cData.SkinImageHeight == 64 && cData.SkinImageWidth == 64)) || parsedSkinDataErr != nil || cData.SkinImageHeight*cData.SkinImageWidth*4 != len(parsedSkinData) || (skinResourcePatch.Geometry.Default != "geometry.humanoid.custom" && skinResourcePatch.Geometry.Default != "geometry.humanoid.customSlim") {
+	if !((cData.SkinImageHeight == 128 && cData.SkinImageWidth == 128) ||
+		(cData.SkinImageHeight == 64 && cData.SkinImageWidth == 64)) ||
+		parsedSkinDataErr != nil ||
+		cData.SkinImageHeight*cData.SkinImageWidth*4 != len(parsedSkinData) ||
+		(skinResourcePatch.Geometry.Default != "geometry.humanoid.custom" && skinResourcePatch.Geometry.Default != "geometry.humanoid.customSlim") {
 		cData.SkinImageHeight = 0
 		cData.SkinImageWidth = 0
 		cData.SkinData = ""
